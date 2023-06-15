cc24ed1706a3ef81354a52765fefafb4c633d769
LazyPeesh <tungthanh254@gmail.com>
2023-06-15 22:25:43 +0700

    minor change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import SearchBox from "./SearchBox";
-import logo from "../assets/logo.png";
+import logo from "../assets/atn_logo.png";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -33,8 +33,8 @@ const Header = () => {
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
-              <img src={logo} alt="ProShop" />
-              ProShop
+              <img src={logo} alt="ATN Shop" />
+              ATN Shop
             </Navbar.Brand>
           </LinkContainer>
 
