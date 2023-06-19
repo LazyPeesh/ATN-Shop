996693aa35af8193d786f1649fadaf8d046da52c
LazyPeesh <tungthanh254@gmail.com>
2023-06-20 04:03:56 +0700

    center carousel component

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -14,9 +14,9 @@ const ProductCarousel = () => {
   ) : (
     <Carousel pause="hover" className="bg-primary mb-4">
       {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+        <Carousel.Item key={product._id} >
+          <Link to={`/product/${product._id}`} className="d-flex justify-content-center">
+            <Image src={product.image} alt={product.name} width={400} className="center-align" fluid />
             <Carousel.Caption className="carousel-caption">
               <h2>
                 {product.name} (${product.price})
